5de4c8213c15a4b8845926f76d2b56043c948f4f
Aditya-Ashtekar <adityaashtekar2012@gmail.com>
2019-12-15 22:03:18 -0500

    Add files via upload

--- rtes_rendezvous/rtes_rendezvous.ino
+++ rtes_rendezvous/rtes_rendezvous.ino
@@ -1,31 +1,35 @@
 #include <ESP8266WiFi.h>
 #include <Servo.h>
 #ifndef WIFIMODULE
-#define WIFISSID "OnePlus 6T"
-#define WIFIPWD  "12345678"
+#define WIFISSID "rtesgroup11"//"OnePlus 6T"
+#define WIFIPWD  "rtesgroup11"//"12345678"
 #endif
 const char* ssid     = WIFISSID;
 const char* password = WIFIPWD;
 Servo servoMotor; //servoMotor for rotating the ultrasonic sensor
-#define STOP_RSSI 30  //If signal for RSSI is below this, it will stop the car
-#define distance_threshold 30
+#define STOP_RSSI -50  //If signal for RSSI is below this, it will stop the car
+#define stop_distance_threshold 20
+#define move_distance_threshold 30
 //Motor A
-#define SERVO 0 // Servo
+#define SERVO 14 // Servo
 #define PWMA 5 //Speed control
-#define AIN1 13 //Direction wheel A
+#define AIN1 0 //Direction wheel A
 #define AIN2 15 //Direction wheel A
 //Motor B
-#define PWMB 2 //Speed control
-#define BIN1 12 //Direction wheel B
-#define BIN2 14 //Direction wheel B
+#define PWMB 4 //Speed control
+#define BIN1 16 //Direction wheel B
+#define BIN2 2 //Direction wheel B
 //#define STNDBY 0 //Standby pin
 // Trig and Echo pin for ultrasonic:
-#define trigPin 4
-#define echoPin 16
+#define trigPin 12
+#define echoPin 13
 
 #define rotateRight 1
 #define rotateLeft 2
-#define rotate360 0
+#define rotate180 0
+
+int forward_count = 0;
+bool stop_bot = false;
 
 void setIOPins(){
   pinMode(AIN1, OUTPUT);
@@ -58,11 +62,11 @@ void setup() {
   servoMotor.attach(SERVO);
   setIOPins();
   Serial.println("Let the Program begin");
-  //connectToWifi();
+  connectToWifi();
 }
 
 int signalStrength(){
-  long rssi = -WiFi.RSSI();
+  long rssi = WiFi.RSSI();
   Serial.print("rssi = ");
   Serial.println(rssi);
   return rssi;
@@ -93,7 +97,7 @@ void turnServo(int angle){
 }
 void getUltrasonicReadings(int *ultrasonicReadingsArray){
   ultrasonicReadingsArray[0] = calculateDistance();
-  turnServo(10);    //Rotate Servo to face Right
+  turnServo(0);    //Rotate Servo to face Right
   ultrasonicReadingsArray[1] = calculateDistance();
   turnServo(180);  //Rotate Servo to face Left
   ultrasonicReadingsArray[2] = calculateDistance();
@@ -113,75 +117,107 @@ void stopCar(){
 void turnCar(int rotateDirection){
   startCar();
   if(rotateDirection == rotateRight){
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, HIGH);
+    delay(1200);
+  }
+  if(rotateDirection == rotateLeft){
     digitalWrite(AIN1, LOW);
     digitalWrite(AIN2, HIGH);
     digitalWrite(BIN1, HIGH);
     digitalWrite(BIN2, LOW);
-    delay(300);
+    delay(1200);
   }
-  if(rotateDirection == rotateLeft){
+  if(rotateDirection == rotate180){
     digitalWrite(AIN1, HIGH);
     digitalWrite(AIN2, LOW);
     digitalWrite(BIN1, LOW);
     digitalWrite(BIN2, HIGH);
-    delay(300);
-  }
-  if(rotateDirection == rotate360){
-    digitalWrite(AIN1, HIGH);
-    digitalWrite(AIN2, LOW);
-    digitalWrite(BIN1, LOW);
-    digitalWrite(BIN2, HIGH);
-    delay(500);
+    delay(2000);
   }
   stopCar();
 }
 
-void moveForward(int distance){
-  startCar();
-  analogWrite(PWMA, 300);
-  analogWrite(PWMB, 350);
+void moveReverse(){
+  Serial.println("Moving Reverse");
+  analogWrite(PWMA, 550);
+  analogWrite(PWMB, 400);
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(BIN2, LOW);
+  delay(500);
+  stopCar();
+}
+int moveForward(int distance){
+//  startCar();
+  Serial.println("Moving Forward");
+  int stuckCount = 50;  //Currently set for 50x400ms(delay in the loop below) = 20seconds
+  analogWrite(PWMA, 550);
+  analogWrite(PWMB, 400);
   digitalWrite(AIN1, LOW);
   digitalWrite(AIN2, HIGH);
   digitalWrite(BIN1, LOW);
   digitalWrite(BIN2, HIGH);
-  while(calculateDistance() > distance_threshold);
+  while(calculateDistance() > stop_distance_threshold && stuckCount > 0){
+    delay(400); //delay between consecutive readings of ultrasonic sensor
+    stuckCount --;
+    Serial.print("RSSI = ");
+    Serial.println(signalStrength());
+    if(signalStrength() > STOP_RSSI){
+        stop_bot = true;
+        break;
+    }
+  }
+  if(stuckCount<=0){
+    return 1; //Car is stuck
+  }
   stopCar();
+  return 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//  if(signalStrength() < STOP_RSSI){
-//    while(1){
-//      Serial.println("Destination Reached!!!!!!!");
-//      delay(1000);
-//    }
-//  }
-  int distance[3];
-  getUltrasonicReadings(distance);
-  Serial.print("Distance Front = ");
-  Serial.println(distance[0]);
-  Serial.print("Distance Right = ");
-  Serial.println(distance[1]);
-  Serial.print("Distance Left = ");
-  Serial.println(distance[2]);
-  if(distance[0] > distance_threshold){
-    moveForward(distance[0]);
-    Serial.println("Moving Forward");
+  if(!stop_bot){  //If destination is not reached, check rssi and move the bot
+    if(signalStrength() > STOP_RSSI){
+        stop_bot = true;
+    }
+    int distance[3];
+    getUltrasonicReadings(distance);
+    Serial.print("Distance Front = ");
+    Serial.println(distance[0]);
+    Serial.print("Distance Right = ");
+    Serial.println(distance[1]);
+    Serial.print("Distance Left = ");
+    Serial.println(distance[2]);
+    if(distance[0] > move_distance_threshold && forward_count <= 3){
+      int stuckFlag = moveForward(distance[0]);
+      if(stuckFlag){
+        forward_count = 4;  //Need to stop the car
+        moveReverse();
+      }
+      forward_count++;
+    }
+    else if(distance[1] > move_distance_threshold && distance[1] > distance[2]){
+      Serial.println("Turning Right");
+      turnCar(rotateRight);
+      forward_count = 0;
+  //    moveForward(distance[1]);
+    }
+    else if(distance[2] > move_distance_threshold){
+      Serial.println("Turning Left");
+      turnCar(rotateLeft);
+      forward_count = 0;
+  //    moveForward(distance[2]);
+    }
+    else{
+      Serial.println("Turning 180");
+      turnCar(rotate180);
+    }
+    delay(2000);
   }
-  else if(distance[1] > distance_threshold){
-    Serial.println("Turning Right");
-    turnCar(rotateRight);
-//    moveForward(distance[1]);
+  else{ //If bot reaches the destination
+    Serial.println("Destination Reached!!!!!!!");
   }
-  else if(distance[2] > distance_threshold){
-    Serial.println("Turning Left");
-    turnCar(rotateLeft);
-//    moveForward(distance[2]);
-  }
-  else{
-    delay(1000);
-    Serial.println("Turning 360");
-    turnCar(rotate360);
-  }
-  delay(5000);
 }
